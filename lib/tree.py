cd8f1631503c8b02d93446f79dca59a1f9d75bff
SC-Kenduiwa <kenduiwashanice64@gmail.com>
2024-06-12 23:22:23 +0300

    complete

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,13 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    def dfs(node):
+            if node['id'] == id:
+                return node
+            for child in node.get('children', []):
+                result = dfs(child)
+                if result:
+                    return result
+            return None
+
+    return dfs(self.root)
